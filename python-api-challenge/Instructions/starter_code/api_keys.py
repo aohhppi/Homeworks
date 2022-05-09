bef5f364725e6fab455cb189f7a1be168355d6b0
aohhppi <98566990+aohhppi@users.noreply.github.com>
2022-05-09 18:48:36 -0400

    Update api_keys.py

--- python-api-challenge/Instructions/starter_code/api_keys.py
+++ python-api-challenge/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "5d5638e928c8e62e3838defe11d1e52f"
+weather_api_key = "5d5638e928c8e62e3838defe11d1e52faohhppi"
 
 # Google API Key
-g_key = "AIzaSyALXm5ZVgxVABYbS3RYO4l0slNV8URupj0"
+g_key = "AIzaSyALXm5ZVgxVABYbS3RYO4l0slNV8URupj0aohhppi"
